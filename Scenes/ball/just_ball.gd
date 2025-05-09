a53d011380253610a24dc06cb11132f52c410d22
MK <mkanyal001@gmail.com>
2025-05-09 22:00:56 +0530

    60%

--- Scenes/ball/just_ball.gd
+++ Scenes/ball/just_ball.gd
@@ -1,6 +1,9 @@
 extends RigidBody2D
 
+@onready var timer = $Timer
+
 func _ready():
+	timer.start(15)
 	randomize()
 	apply_random_impulse()
 
@@ -17,3 +20,8 @@ func _physics_process(delta):
 func _on_area_2d_body_entered(body):
 	if body.is_in_group("player"):
 		GlobalBeings.emit_signal("red",10)
+	if body.is_in_group('red_block'):
+		body.queue_free()
+
+func _on_timer_timeout():
+	$".".queue_free()
