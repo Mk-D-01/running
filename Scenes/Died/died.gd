08dd5b47ada9a54df26bcfa9adfad2fab60c8a1a
MK <mkanyal001@gmail.com>
2025-05-10 16:10:06 +0530

    ty

--- Scenes/Died/died.gd
+++ Scenes/Died/died.gd
@@ -5,14 +5,24 @@ var score = GlobalBeings.score
 var name___ = 'd'
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	score = 10
+	
+	
+	score = int(randf_range(1,10))
+	print(score)
+	var name = str(int(randf_range(0,9999)) )
+	$Timer.start(0.5)
 	pass # Replace with function body.
-	label_2 = "Score" + str(score)
-	await Leaderboards.post_guest_score("runnin-longest-runner-Sfwq",score+10+10,name___)
-	$LeaderboardUI.show()
+	label_2.text = "Score " + str(score)
+	await Leaderboards.post_guest_score("runnin-longest-runner-Sfwq",45,"dfdfd")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 func _input(event):
 	if Input.is_action_just_pressed("jump"):
+		print('refresh')
 		get_tree().reload_current_scene()
+
+
+func _on_timer_timeout():
+	pass
+	pass # Replace with function body.
